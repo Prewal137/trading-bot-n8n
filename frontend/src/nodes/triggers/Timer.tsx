ce8ba046fe1adff9a5a55ae0fbdda60b1eabe133
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-14 14:14:34 +0530

    Added Timer.tsx file

---
+++ frontend/src/nodes/triggers/Timer.tsx
@@ -0,0 +1,17 @@
+import { Handle, Position } from "@xyflow/react";
+type TimerNodeMetadata ={
+    time: string;
+};
+
+
+export function Timer({ data, isConnectable }: {
+   data: {
+      metadata: TimerNodeMetadata
+   };
+   isConnectable: boolean;
+}) {
+   return <div className="p-4 border">
+         {data.metadata.time}
+        <Handle type="source" position={Position.Right} isConnectable={isConnectable}></Handle>
+    </div>
+}
