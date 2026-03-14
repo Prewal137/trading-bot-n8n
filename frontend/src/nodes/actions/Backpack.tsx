340b596391e7edd659d231da87c0b05ba08185dd
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-14 22:13:03 +0530

    Created Backpack and Hyperliquid files

---
+++ frontend/src/nodes/actions/Backpack.tsx
@@ -0,0 +1,24 @@
+import { SUPPORTED_ASSETS } from "@/components/TriggerSheet";
+import {Handle, Position} from "@xyflow/react";
+export type TradingMetadata = {
+  type: "LONG" | "SHORT";
+  qty: number;
+  symbol: typeof SUPPORTED_ASSETS;
+};
+
+export function Lighter({ data }: {
+  data: {
+    metadata: TradingMetadata;
+  };
+}) {
+  return (
+    <div className="p-4 border">
+      Lighter Trade
+      <div>{data.metadata.type}</div>
+      <div>{data.metadata.qty}</div>
+      <div>{data.metadata.symbol}</div>
+      <Handle type="source" position={Position.Right}></Handle>
+      <Handle type="target" position={Position.Left}></Handle>
+    </div>
+  );
+}
