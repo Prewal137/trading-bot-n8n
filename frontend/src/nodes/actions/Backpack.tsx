60b65c8cb0c37cd084b03d60155ec21939419e11
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-14 22:24:52 +0530

    Written code of Backpack.tsx

--- frontend/src/nodes/actions/Backpack.tsx
+++ frontend/src/nodes/actions/Backpack.tsx
@@ -1,19 +1,15 @@
 import { SUPPORTED_ASSETS } from "@/components/TriggerSheet";
 import {Handle, Position} from "@xyflow/react";
-export type TradingMetadata = {
-  type: "LONG" | "SHORT";
-  qty: number;
-  symbol: typeof SUPPORTED_ASSETS;
-};
+import type { TradingMetadata } from "./Lighter";
 
-export function Lighter({ data }: {
+export function Backpack({ data }: {
   data: {
     metadata: TradingMetadata;
   };
 }) {
   return (
     <div className="p-4 border">
-      Lighter Trade
+      Backpack Trade
       <div>{data.metadata.type}</div>
       <div>{data.metadata.qty}</div>
       <div>{data.metadata.symbol}</div>
