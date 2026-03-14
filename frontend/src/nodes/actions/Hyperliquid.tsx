b900d87fb8ee4645ec09ae0ded65a640a7e283ef
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-14 22:18:30 +0530

    Written Hyperliquid.tsx code

--- frontend/src/nodes/actions/Hyperliquid.tsx
+++ frontend/src/nodes/actions/Hyperliquid.tsx
@@ -1,19 +1,15 @@
 import { SUPPORTED_ASSETS } from "@/components/TriggerSheet";
 import {Handle, Position} from "@xyflow/react";
-export type TradingMetadata = {
-  type: "LONG" | "SHORT";
-  qty: number;
-  symbol: typeof SUPPORTED_ASSETS;
-};
+import type { TradingMetadata } from "./Lighter";
 
-export function Lighter({ data }: {
+export function Hyperliquid({ data }: {
   data: {
-    metadata: TradingMetadata;
+    metadata: TradingMetadata
   };
 }) {
   return (
     <div className="p-4 border">
-      Lighter Trade
+      Hyperliquid Trade
       <div>{data.metadata.type}</div>
       <div>{data.metadata.qty}</div>
       <div>{data.metadata.symbol}</div>
