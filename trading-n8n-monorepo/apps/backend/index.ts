29ce255abb09efd1acd2bdc33a9c3a426c1f1d02
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-17 12:21:21 +0530

    Made some backend changes

--- trading-n8n-monorepo/apps/backend/index.ts
+++ trading-n8n-monorepo/apps/backend/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
@@ -9,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log("Server running at http://localhost:3001");
+  console.log("Server running on port 3001");
 });
