5560c20d880584a17a86ad8608c4fd36cfc5c986
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-19 15:29:18 +0530

    wrote createworflow logic in index.ts

--- trading-n8n-monorepo/packages/common/types/index.ts
+++ trading-n8n-monorepo/packages/common/types/index.ts
@@ -7,3 +7,22 @@ export const SigninSchema = z.object({
     username: z.string().min(3).max(100),
     password: z.string().min(6)
 })
+export const CreateWorkflowSchema = z.object({
+    nodes:z.array(z.object({
+        type:z.string(),
+        data:z.object({
+            kind:z.enum(["action","trigger"]),
+            metadata:z.any()
+        }),
+        id:z.string(),
+        position:z.object({
+            x:z.number(),
+            y:z.number()
+        })
+    })),
+    edges:z.array(z.object({
+        id:z.string(),
+        source:z.string(),
+        target:z.string()
+    }))
+})
