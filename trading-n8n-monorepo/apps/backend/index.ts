55b7018e8bf9a4f3811250476cebae5900b4aac5
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-19 15:21:18 +0530

    Added few lines of code

--- trading-n8n-monorepo/apps/backend/index.ts
+++ trading-n8n-monorepo/apps/backend/index.ts
@@ -69,8 +69,8 @@ app.post("/signin", async (req: Request, res: Response) => {
 });
 
 app.post("/workflow", authMiddleware, (req, res) => {
-  const userId = req.userId;
-  
+  const userId = req.userId!;
+
 
 });
 
