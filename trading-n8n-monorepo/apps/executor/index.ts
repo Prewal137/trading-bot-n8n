3e670cdc7a96f4098a448a6a8b66bbfef8482fdd
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-03-20 18:46:48 +0530

    writing the logic for execution

--- trading-n8n-monorepo/apps/executor/index.ts
+++ trading-n8n-monorepo/apps/executor/index.ts
@@ -3,7 +3,24 @@ import { WorkflowModel } from "@repo/db";
 async function main(){
 
 while(1){
-   await WorkflowModel.find({});
+   const workflows=await WorkflowModel.find({});
+   workflows.map(async(workflow)=>{
+    const trigger = workflow.nodes.find(x=>x.data?.kind==="TRIGGER");
+    if (!trigger){
+        return;
+    }
+
+    switch (trigger?.type){
+        case "timer":
+            const timeInS = trigger.data?.metadata.time;
+            const execution = await ExecutionModel.findOne({
+                workflowId: worklfow.id,
+            }).sort({
+                startTime: 1
+            })
+            if (execution.startTime <= )
+    }
+   })
 }
 
 
